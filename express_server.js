db3e4f14e5fce03786cb389930674cc83fa7fc8c
ffiargus <rm.chow@outlook.com>
2017-05-10 17:02:03 +0000

    Completes login feature

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+var cookieParser = require('cookie-parser')
 const bodyParser = require("body-parser");
 
+app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 function generateRandomString() {
@@ -25,10 +28,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+                       username: req.cookies["username"]
+                     };
+  res.render("urls_new", templateVars);
 });
 
-app.use(express.static(__dirname + '/views'));
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // debug statement to see POST parameters
@@ -39,9 +44,15 @@ app.post("/urls", (req, res) => {
   // res.render("/urls_index");
 });
 
+app.get("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 app.post("/urls/:id/delete", (req, res) => {
   console.log(req.body);  // debug statement to see POST parameters
   delete urlDatabase[req.params.id]
+  console.log("lighhouse sucks")
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
@@ -52,25 +63,35 @@ app.post("/urls/:id", (req, res) => {
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+
 app.get("/u/:shortURL", (req, res) => {
   let longURL = req.params.shortURL;
   res.redirect(urlDatabase[longURL]);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase,
+                       username: req.cookies["username"]
+                     };
   res.render("urls_index", templateVars);
 })
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
-                       longURL: urlDatabase[req.params.id]
+                       longURL: urlDatabase[req.params.id],
+                       username: req.cookies["username"]
                      };
   res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+  //res.end("<html><body>Hello <b>World</b></body></html>\n");
+  res.redirect("/urls/");
 })
 
 app.listen(PORT, () => {
